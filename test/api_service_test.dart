fbd890935382e594b4955d8b40de94dca7a8c5c8
MalinduDilshan11 <dilshanmalindu11@gmail.com>
2026-03-08 16:52:44 +0530

    Test api service

---
+++ test/api_service_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:diapredict/services/api_service.dart';
+
+void main() {
+
+  test('Signup API returns success', () async {
+
+    final result = await ApiService.signup(
+      'Test User',
+      'test@test.com',
+      '123456'
+    );
+
+    expect(result['success'], true);
+
+  });
+
+}
